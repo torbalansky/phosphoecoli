5ccfaf5690cfcceb1ddba9c268823b663dfa143a
torbalansky <torbalansky@gmail.com>
2024-04-13 13:41:16 +0100

    Run collectstatic and update static files

--- ecoliphos/settings.py
+++ ecoliphos/settings.py
@@ -108,8 +108,8 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
