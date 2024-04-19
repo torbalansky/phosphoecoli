e08dd94b704e7b4edff5748481c5c898efd15207
torbalansky <torbalansky@gmail.com>
2024-04-19 09:47:38 +0100

    Updated protein-list search functionalities. Added help text.

--- ecoliphos/settings.py
+++ ecoliphos/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config('SECRET_KEY')
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['pedb.onrender.com', '127.0.0.1', 'localhost']
 
